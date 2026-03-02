ca6a77e1503a1abfe0325ddc36577336918df46e
Gepsiu <d_piotrek20@wp.pl>
2026-03-02 12:02:52 +0100

    Created Measurement model

--- air_quality_evolution_map/map/models.py
+++ air_quality_evolution_map/map/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Measurement(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    pollutant = models.CharField(max_length=20)
+    measurement_date = models.DateField()
+    station = models.CharField(max_length=30)
+    measurement_value = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        managed = False
+        db_table = "measurements"
