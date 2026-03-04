5d4e59a24659da6058ebd0be7153d4abc6dccbd7
Gepsiu <d_piotrek20@wp.pl>
2026-03-04 14:17:17 +0100

    Created 2 materialized views

--- air_quality_evolution_map/map/models.py
+++ air_quality_evolution_map/map/models.py
@@ -11,3 +11,27 @@ class Measurement(models.Model):
     class Meta:
         managed = False
         db_table = "measurements"
+
+
+class MonthlyMeasurement(models.Model):
+    id = models.IntegerField(db_column="month_id", primary_key=True)
+    station = models.CharField(db_column="station", max_length=30)
+    pollutant = models.CharField(db_column="pollutant", max_length=20)
+    month_start = models.DateField(db_column="month_start")
+    avg_value = models.DecimalField(db_column="avg_value", max_digits=10, decimal_places=2)
+
+    class Meta:
+        managed = False
+        db_table = "monthly_measurement"
+
+
+class YearlyMeasurement(models.Model):
+    id = models.IntegerField(db_column="year_id", primary_key=True)
+    station = models.CharField(db_column="station", max_length=30)
+    pollutant = models.CharField(db_column="pollutant", max_length=20)
+    year_start = models.DateField(db_column="year_start")
+    avg_value = models.DecimalField(db_column="avg_value", max_digits=10, decimal_places=2)
+
+    class Meta:
+        managed = False
+        db_table = "yearly_measurement"
