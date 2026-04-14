d8e93b358e1c40f4e7dc626bfdcb592030c62f85
Gepsiu <d_piotrek20@wp.pl>
2026-04-14 15:11:36 +0200

    New class Station with FK from Measurement

--- air_quality_evolution_map/map/models.py
+++ air_quality_evolution_map/map/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 
+class Station(models.Model):
+    code = models.CharField(max_length=40, primary_key=True)
+    voivodeship = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.code
+
+
 class Measurement(models.Model):
     id = models.IntegerField(primary_key=True)
     pollutant = models.CharField(max_length=20)
     measurement_date = models.DateField()
-    station = models.CharField(max_length=30)
+    station = models.ForeignKey(Station, on_delete=models.PROTECT, db_column="station")
     measurement_value = models.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
