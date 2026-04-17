a86bb41291d330922955e032963ffbd35aaa8c67
Gepsiu <d_piotrek20@wp.pl>
2026-04-17 12:22:05 +0200

    Finally db model

--- air_quality_evolution_map/map/models.py
+++ air_quality_evolution_map/map/models.py
@@ -14,25 +14,10 @@ class Station(models.Model):
 
 
 class Measurement(models.Model):
-    id = models.IntegerField(primary_key=True)
     pollutant = models.CharField(max_length=20)
     measurement_date = models.DateField()
     station = models.ForeignKey(Station, on_delete=models.PROTECT, db_column="station")
     measurement_value = models.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
-        managed = False
         db_table = "measurements"
-
-
-class MeasurementAgg(models.Model):
-    id = models.IntegerField(db_column="id", primary_key=True)
-    station = models.CharField(db_column="station", max_length=30)
-    pollutant = models.CharField(db_column="pollutant", max_length=20)
-    start_date = models.DateField(db_column="start_date")
-    avg_value = models.DecimalField(db_column="avg_value", max_digits=10, decimal_places=2)
-
-    class Meta:
-        managed = False
-        db_table = "measurement_agg"
-        indexes = [models.Index(fields=["pollutant", "start_date"], name="idx_measurement_agg")]
