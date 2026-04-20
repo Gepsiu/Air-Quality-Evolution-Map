910d4d3aff0be3cef71349c3e7faec502fc344c9
Gepsiu <d_piotrek20@wp.pl>
2026-04-20 11:35:00 +0200

    Mainly model with Station and Measurement class FK and MaterializedView

--- air_quality_evolution_map/map/models.py
+++ air_quality_evolution_map/map/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Station(models.Model):
     code = models.CharField(max_length=40, primary_key=True)
-    outdated_code = models.CharField(max_length=40, blank=True, null=True)
     voivodeship = models.CharField(max_length=20)
 
     def __str__(self):
@@ -25,7 +24,7 @@ class Measurement(models.Model):
 
 class MeasurementAgg(models.Model):
     id = models.IntegerField(db_column="id", primary_key=True)
-    voivodeship = models.CharField(db_column="station", max_length=20)
+    voivodeship = models.CharField(db_column="voivodeship", max_length=20)
     pollutant = models.CharField(db_column="pollutant", max_length=20)
     start_date = models.DateField(db_column="start_date")
     avg_value = models.DecimalField(db_column="avg_value", max_digits=10, decimal_places=2)
