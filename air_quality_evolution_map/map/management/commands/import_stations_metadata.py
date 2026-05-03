3cc65bf75d1e63f3033f1cb9cc2cefe134e4c2af
Gepsiu <d_piotrek20@wp.pl>
2026-05-03 17:07:26 +0200

    Add script to manage.py runner

---
+++ air_quality_evolution_map/map/management/commands/import_stations_metadata.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+
+from map.scripts import stations_metadata_loader
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.SUCCESS("Script started..."))
+        try:
+            stations_metadata_loader.main()
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f"ERROR: {e}"))
+        self.stdout.write(self.style.SUCCESS("Finished importing pollution data"))
