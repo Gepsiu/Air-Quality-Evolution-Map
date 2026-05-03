504be0955e5fec8c4fd5e8ae3e597a9c7429329a
Gepsiu <d_piotrek20@wp.pl>
2026-05-03 17:07:16 +0200

    Add script to manage.py runner

--- air_quality_evolution_map/map/management/commands/import_pollution_data.py
+++ air_quality_evolution_map/map/management/commands/import_pollution_data.py
@@ -1,10 +1,14 @@
 from django.core.management.base import BaseCommand
 
-from map.scripts import file_converter_to_df
+from map.scripts import pollution_data_loader
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        file_converter_to_df.main()
+        self.stdout.write(self.style.SUCCESS("Script started..."))
+        try:
+            pollution_data_loader.main()
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f"Error: {e}"))
         self.stdout.write(self.style.SUCCESS("Finished importing pollution data"))
