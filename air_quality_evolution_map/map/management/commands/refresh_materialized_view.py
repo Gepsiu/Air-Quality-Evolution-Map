f70e4a340ebd3e522c989e7b5b7d01bba10d3606
Gepsiu <d_piotrek20@wp.pl>
2026-05-07 12:26:33 +0200

    Add info to MV script

--- air_quality_evolution_map/map/management/commands/refresh_materialized_view.py
+++ air_quality_evolution_map/map/management/commands/refresh_materialized_view.py
@@ -4,5 +4,7 @@ from django.db import connection
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
+        self.stdout.write(self.style.SUCCESS("Script started..."))
         with connection.cursor() as cursor:
             cursor.execute("REFRESH MATERIALIZED VIEW measurement_agg;")
+        self.stdout.write(self.style.SUCCESS("Finished!"))
